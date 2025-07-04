0d8b1ba511fc55431c1293740a348d5cd7f5e3e3
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-04 16:01:17 +0600

    Index ja 🏀

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
-// index js
+// Index js
 const app = express()
 app.use(express.json())
 app.use(cors())
